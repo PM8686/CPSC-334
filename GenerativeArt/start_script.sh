031393b1151b309b6dcb5de8a9508f636854e744
PM8686 <petermessina86@gmail.com>
2024-09-16 22:14:41 -0400

    startup full screen

--- GenerativeArt/start_script.sh
+++ GenerativeArt/start_script.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-# Step 1: Open a terminal
-# Step 2: Navigate to the desired directory
+# Open a terminal
+# Navigate to the desired directory
 cd /home/student334/Desktop/CS334/GenerativeArt || { echo "Directory not found"; exit 1; }
 
-# Step 3: Start the Python HTTP server on port 7001
-python -m http.server 7001 &
+# Start the Python HTTP server on port 7002
+python -m http.server 7002 &
 
 # Give the server a moment to start
 sleep 2
 
-# Step 4: Open Chromium with the local server URL
+# make default browser size full screen
+chromium-browser --new-window --start-fullscreen
+pkill chromium-browser
+
+# Open Chromium with the local server URL
 # Start Chromium in the background
-chromium-browser --new-window  http://localhost:7001 
-
-# # Step 5: Use wmctrl to make the Chromium window fullscreen
-# # Get the window ID of the Chromium tab
-# WINDOW_ID=$(wmctrl -lx | grep 'chromium-browser.Chromium-browser' | awk '{print $1}')
-
-# # Check if we got a window ID
-# if [ -n "$WINDOW_ID" ]; then
-#     wmctrl -ir "$WINDOW_ID" -b toggle,fullscreen
-# else
-#     echo "Chromium window not found"
-# fi
+chromium-browser --new-window  http://localhost:7002 
 
 
 # may need to use the below

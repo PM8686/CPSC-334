c5c392c8756b27e2c08572e34b2ce65236d49bf3
PM8686 <petermessina86@gmail.com>
2024-09-16 21:37:01 -0400

    edit bash file port

--- GenerativeArt/start_script.sh
+++ GenerativeArt/start_script.sh
@@ -5,17 +5,11 @@
 cd /home/student334/Desktop/CS334/GenerativeArt || { echo "Directory not found"; exit 1; }
 
 # Step 3: Start the Python HTTP server on port 8000
-python -m http.server 8000 &
+python -m http.server 7000 
 
 # Step 4: Open the default web browser with the local server URL
-xdg-open http://localhost:8000
-
-# Step 5: Wait for the browser to open, then simulate pressing F11 to go full screen
-sleep 5  # Wait for the browser to launch
-xdotool key F11
-
+xdg-open http://localhost:7000
 
 # may need to use the below
-# sudo apt-get install xdotool
 # chmod +x start_server.sh
 # ./start_server
